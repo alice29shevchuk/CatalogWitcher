6d6f1208e6168ce1178f406aee5d5b08598ef10f
alice29shevchuk <95874389+alice29shevchuk@users.noreply.github.com>
2022-11-30 17:12:59 +0200

    test card view

--- View/ListCharacters.xaml.cs
+++ View/ListCharacters.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CatalogWitcher.Model.CustomControls;
+using CatalogWitcher.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,19 +12,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CatalogWitcher.Model.CustomControls
+namespace CatalogWitcher.View
 {
     /// <summary>
-    /// Логика взаимодействия для CharacterControl.xaml
+    /// Логика взаимодействия для ListCharacters.xaml
     /// </summary>
-    public partial class CharacterControl : UserControl
+    public partial class ListCharacters : Window
     {
-        public CharacterControl()
+        public ListCharacters()
         {
             InitializeComponent();
+            this.DataContext = new ListCharactersViewModel();
         }
     }
 }
---
+++ ViewModel/ListCharactersViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CatalogWitcher.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogWitcher.ViewModel
+{
+    public class ListCharactersViewModel : INotifyCollectionChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        private ObservableCollection<Character> character;
+
+        public ObservableCollection<Character> Character
+        {
+            get { return character; }
+            set { character = value; }
+        }
+        public ListCharactersViewModel()
+        {
+            Character = new ObservableCollection<Character>(new ModelContext().Characters);
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
